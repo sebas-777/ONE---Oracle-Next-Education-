da174aea663c91c70e80125b433fdc7c540b555d
Sebastian Castro <scastro@qvision.com.co>
2024-01-18 21:02:42 -0500

    condicionales if y else

--- Logica_de_programción_JS/2034-logica-programacion-1-ProyectoInicial/app.js
+++ Logica_de_programción_JS/2034-logica-programacion-1-ProyectoInicial/app.js
@@ -1,19 +1,18 @@
 alert('Bienvenida y bienvenida a nuestro sitio web');
-// let numeroUsuario = prompt("Elige un numero entre 1 y 10");
-// let numeroSecreto = 6;
-// console.log(numeroUsuario);
-// if(numeroUsuario == numeroSecreto){
-//     alert('Acertaste el numero ')
-// } 
-let nombre = "luna";
-let edad = 25;
-let numeroDeVentas = 50;
-let saldoDisponible = 1000;
-alert("¡Error! Completa todos los campos");
-let mensajeDeError  = "¡Error! Completa todos los campos";
-alert(mensajeDeError);
-nombre = prompt('Cual es tu nombre');
-edad = prompt('Ingrese su edad');
-if(edad >= 18){
-    alert( "¡Puedes obtener tu licencia de conducir!");
+
+//Variables
+let numeroUsuario = prompt("Elige un numero entre 1 y 10");
+let numeroSecreto = 6;
+
+console.log(numeroUsuario);
+/*
+comparacion
+*/
+if(numeroUsuario == numeroSecreto){
+    //Acertamos, fue verdadera la condicion
+    alert(`Acertaste el numero : ${numeroUsuario}`)
+} else{
+    // la condicion es falsa 
+    alert('Lo siento, no acertastes el numero ')
 }
+
