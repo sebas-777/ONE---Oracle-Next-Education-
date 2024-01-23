10ef47e0c009c6e83390434d7f69ca9e7570e171
Sebastian Castro <scastro@qvision.com.co>
2024-01-22 21:59:39 -0500

    finalizacion de primer curso

--- Logica_de_programción_JS/2034-logica-programacion-1-ProyectoInicial/app.js
+++ Logica_de_programción_JS/2034-logica-programacion-1-ProyectoInicial/app.js
@@ -1,18 +1,37 @@
 alert('Bienvenida y bienvenida a nuestro sitio web');
-
 //Variables
-let numeroUsuario = prompt("Elige un numero entre 1 y 10");
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario =0;
+let intentos = 1;
+let maximoIntentos = 3;
+let numeroMaximoPosible = 10;
 
-console.log(numeroUsuario);
-/*
-comparacion
-*/
-if(numeroUsuario == numeroSecreto){
-    //Acertamos, fue verdadera la condicion
-    alert(`Acertaste el numero : ${numeroUsuario}`)
-} else{
-    // la condicion es falsa 
-    alert('Lo siento, no acertastes el numero ')
+while (numeroUsuario != numeroSecreto ) { 
+
+    let numeroUsuario =parseInt(prompt(`Ingresa un numero entre 1 y ${numeroMaximoPosible} `));
+        
+    /*
+    comparacion
+    */
+    if(numeroUsuario == numeroSecreto){
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste el numero : ${numeroUsuario}. Lo hicistes en ${intentos} ${intentos == 1 ? "vez" : "veces"}`);
+    } else{
+        if (numeroUsuario > numeroSecreto){
+        // numero usuario menor 
+             alert('El numero secreto es menor ');
+        }else{
+            // numero usuario mayor 
+            alert('El numero secreto es mayor ')
+        }
+          //Incrementamos el contador cuando no acierta 
+        intentos++; 
+        if(intentos > maximoIntentos){
+            alert(`Llegastes al numero Maximo de ${maximosIntentos} intentos`);
+            break;
+        }      
+    }
+
 }
+
 
