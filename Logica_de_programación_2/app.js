03331046eeea85e6704cb2e69a3db186ee01488c
Sebastian Castro <scastro@qvision.com.co>
2024-01-24 21:11:25 -0500

    Inicializacion del proyecto

---
+++ Logica_de_programación_2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del Numero Secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
