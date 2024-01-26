64420e8d0825e32b589bb434253a9d395cb751c8
Sebastian Castro <scastro@qvision.com.co>
2024-01-25 21:18:16 -0500

    funciones

--- Logica_de_programación_2/app.js
+++ Logica_de_programación_2/app.js
@@ -3,3 +3,8 @@ titulo.innerHTML = 'Juego del Numero Secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+
+function intentoDeUsuario(){
+    alert('Click desde el boton');
+}
