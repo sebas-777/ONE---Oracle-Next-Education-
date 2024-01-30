ca15be331c780a87195f5931eff77f2e1212fbf9
Sebastian Castro <scastro@qvision.com.co>
2024-01-30 00:03:03 -0500

    funciones

--- Logica_de_programación_2/app.js
+++ Logica_de_programación_2/app.js
@@ -1,7 +1,9 @@
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0;
+let intentos = 0;
 
 console.log('Numero secreto: ', numeroSecreto);
-console.log('Numero aleatorio: ', Math.floor(Math.random()*10));
+
+
 
 function asignarTextoElemento(elemento,texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -11,18 +13,47 @@ function asignarTextoElemento(elemento,texto) {
 
 function verificarIntento(){
     let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log('tipo de dato numero usuario:' ,typeof(numeroUsuario));
-    console.log('tipo de dato numero secereto:' ,typeof(numeroSecreto));
-    console.log(numeroUsuario);
-    console.log('Numero secreto: ', numeroSecreto);
-    console.log('Comparacion de numeros :', numeroSecreto == numeroUsuario);
-
+   
+    
+    if(numeroSecreto === numeroUsuario){
+        asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if (numeroUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El numero secreto es menor');
+        }else{
+            asignarTextoElemento('p','El numero secreto es mayor');
+        }
+        intentos++;
+        limpiarCampos();
+    }
+    return;
 } 
+
+function limpiarCampos() {
+    document.querySelector('#valorUsuario').value = '';
+}
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
-   
 }
 
-asignarTextoElemento('H1','Juego del Numero Secreto!');
-asignarTextoElemento('p','Indica un numero del 1 al 10');
+function condicionesIniciales() {
+    asignarTextoElemento('H1','Juego del Numero Secreto!');
+    asignarTextoElemento('p','Indica un numero del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCampos();
+    //Indicar mensaje de intervalo de numeros 
+    //Generar el numero aleatorio
+    //Inicializar el numero intentos
+    condicionesIniciales();
+    //Deshabilitar el boton de nuevo juego 
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+
+}
+
+condicionesIniciales();
